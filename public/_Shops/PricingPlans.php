2b59acf67cdfee848f8f13d16143600c8906d548
AtsMurtuza <murtuza05@gmail.com>
2014-05-14 12:27:53 +0530

    Pricing Plans UI design
    UI design for the pricing plans on the first page

---
+++ public/_Shops/PricingPlans.php
@@ -0,0 +1,160 @@
+<style type="text/css">
+	.pricing-first
+	{
+		text-align:center;
+	}
+	.pricing-first:hover
+	{
+		border:5px #F2EDA7 solid;
+		box-shadow: -10px 10px 5px #F2F8DC;
+	}
+	.pricing-first-header
+	{
+		background-color:#F2EDA7;
+		color: black;
+	}
+	
+	.pricing-second
+	{
+		text-align:center;
+	}
+	.pricing-second:hover
+	{
+		border:5px #F2D49B solid;
+		box-shadow: -10px 10px 5px #F2CD85;
+	}
+	.pricing-second-header
+	{
+		background-color:#F2D49B;
+		color: black;
+	}
+	.pricing-third
+	{
+		text-align:center;
+		border:5px #6883A6 solid;
+		box-shadow: -10px 10px 5px #425369;
+	}
+	.pricing-badge
+	{
+		color:#88C1F2;
+		font-size:18px;
+		margin-top:-40px;
+		background-color:#D9D9D9;
+		border:2px #D9D9D9 solid;
+		border-radius:5em;
+		width:100px;
+		height:30px;
+		position:relative;
+		top:-23;
+		right:-225;
+		-ms-transform: rotate(45deg); /* IE 9 */
+		-webkit-transform: rotate(45deg); /* Chrome, Safari, Opera */
+		transform: rotate(45deg);
+	}
+	.pricing-third-header
+	{
+		background-color:#6883A6;
+		color: white;
+	}
+	.pricing-forth
+	{
+		text-align:center;
+	}
+	.pricing-forth:hover
+	{
+		border:5px #233559 solid;
+		box-shadow: -10px 10px 5px #3E5E9E;
+	}
+	.pricing-forth-header
+	{
+		background-color:#233559;
+		color:white;
+	}
+</style>
+<div class="col-xs-12" style="min-height:600px;">
+	<div class="col-md-3">
+		<div class="list-group pricing-first">
+			<div class="list-group-item pricing-first-header">
+				<h1>Basic</h1>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="list-group pricing-second">
+			<div class="list-group-item pricing-second-header">
+				<h1>Basic</h1>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="list-group pricing-third">
+			<div class="list-group-item pricing-third-header">
+				<h1>Basic</h1>	
+				<p class="pricing-badge"><i class="fa fa-gift"></i>&nbsp;Offer&nbsp;<i class="fa fa-gift"></i></p>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="list-group pricing-forth">
+			<div class="list-group-item pricing-forth-header">
+				<h1>Basic</h1>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+			<div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div><div class="list-group-item">
+				<h4>Get Prices</h4>
+			</div>
+		</div>
+	</div>	
+</div>
