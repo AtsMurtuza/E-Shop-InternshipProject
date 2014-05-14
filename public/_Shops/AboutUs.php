d787d3516bd0a5c6c00af0de4a7d2a152b678a0c
AtsMurtuza <murtuza05@gmail.com>
2014-05-14 12:26:55 +0530

    About Us UI design
    About Us UI design for main page

---
+++ public/_Shops/AboutUs.php
@@ -0,0 +1,12 @@
+<div class="col-xs-12" style="height:600px;">
+	<div class="col-md-4" >
+		<iframe id="player" style="box-shadow: 10px 10px 5px #888888;border-radius:5%;border:5px solid gray;" type="text/html" width="100%" height="300px" src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com" frameborder="0"></iframe>
+	</div>
+	<div class="col-md-8">
+		<h1>About Us</h1>
+		<h4 style="text-align:right;margin-top:-60px"><a class="btn btn-lg btn-success">See More</a></h4>
+		<h3 style="border-top:2px solid gray;padding:5px;">
+		Providing E-Commerce Solution is not just our business it's our hobby
+		</h3>
+	</div>
+</div>
