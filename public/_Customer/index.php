c59a30a2fc67b8cda5d9756e516835ca5bed02da
AtsMurtuza <murtuza05@gmail.com>
2014-06-02 13:10:20 +0530

    Badges & Stuff Customer

--- public/_Customer/index.php
+++ public/_Customer/index.php
@@ -3,11 +3,17 @@
 		<title>E-Shop</title>
 		<?php require_once('_css-include.php');?>
 		<!--Page Specific -->
+		<style>
+			.badge{
+				background-color:green;
+			}
+		</style>
 	</head>
-	<body style="padding-left:15px;">
+	<body>
 	<br>
 	<div>
-	<div style="z-index:5" class="list-group col-xs-2">
+	<div style="z-index:5" class="col-xs-2">
+		<div class="list-group">
 		<a href="#" class="list-group-item active"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
@@ -15,18 +21,21 @@
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
+		</div>
 	</div>
-	<div class="col-xs-8">
+	<div class="col-xs-8" style="left:7">
 		<?php require_once('DealsInfo.php');?>
 	</div>
-	<div style="z-index:5" class="list-group col-xs-2">
+	<div style="z-index:5" class="col-xs-2">
+		<div class="list-group">
 		<a href="#" class="list-group-item active"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
 		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
-		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1</h4></a>
+		<a href="#" class="list-group-item"><h4 style="text-align:center">Category 1<span class="pull-right badge">14</span></h4></a>
+		</div>
 	</div>
 	</div>
 	<?php require_once('unloggedheader.php');?>
